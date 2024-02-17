aaafcc64de78ed7d94aba57b0d7c8f3eb03aec29
zillulofficial <zillul.hakim100@gmail.com>
2024-02-17 22:06:41 +0600

    game build done

--- script/index.js
+++ script/index.js
@@ -1,20 +1,78 @@
-function startGaming(){
-    // calling to generate a random alphabet
-    const Num=getRandomAlphabet()
-    console.log(Num)
+document.addEventListener('keyup', function (event) {
+    const PlayerPressed = event.key
 
-    const Current= document.getElementById('current-alphabet')
-    Current.innerText= Num
+    // get the already created random alphabet 
+    const randomAlphabet = this.getElementById('current-alphabet')
+    const expected = randomAlphabet.innerText.toLowerCase()
+
+    // exit the game
+    if(PlayerPressed === 'Escape'){
+        gameOver()
+    }
+    // check if the pressed key is right
+    if (expected === PlayerPressed) {
+        removeColorOfTheAlphabet(PlayerPressed)
+        // get the score
+        const currentScore = getTextElementValueById('current-score')
+
+        // update it
+        let newScore = currentScore + 1
+
+        // show it
+        setTextElementValueById('current-score', newScore)
+
+        startGaming()
+    }
+    else {
+        // get the life
+        const currentLife = getTextElementValueById('current-life')
+        // update it
+        const newLife = currentLife - 1
+        // show it
+        setTextElementValueById('current-life', newLife)
+        if (newLife === 0) {
+            gameOver()
+        }
+    }
+})
+
+function startGaming() {
+    // calling to generate a random alphabet
+    const Num = getRandomAlphabet()
+
+    const Current = document.getElementById('current-alphabet')
+    Current.innerText = Num
 
     // set color 
     setColorOfTheAlphabet(Num)
 }
-function play(){
-    // hiding home screen
+function play() {
+    // hiding section
     hideElementById('home-screen')
-    // showing play screen
+    hideElementById('score-card')
     showElementById('play-ground')
-    // calling to start random alphabet function
+
+    // reset score and life
+    setTextElementValueById('current-life', 5)
+    setTextElementValueById('current-score', 0)
+
     startGaming()
 }
+function gameOver() {
+    hideElementById('play-ground')
+    showElementById('score-card')
+
+    // showing the final result 
+    // get the final result
+    const getResult= document.getElementById('current-score')
+    const result= getResult.innerText
+    const lastResult= setTextElementValueById('last-score', result)
+
+    // removing highlight
+    const clearHighlight= document.getElementById('current-alphabet')
+    const removeHighlight= clearHighlight.innerText
+    removeColorOfTheAlphabet(removeHighlight)
+}
+
+
 
--- script/utility.js
+++ script/utility.js
@@ -1,8 +1,9 @@
+// hide section
 function hideElementById(elementId) {
     const element = document.getElementById(elementId)
     element.classList.add('hidden')
 }
-
+// show section
 function showElementById(anotherElement) {
     const element = document.getElementById(anotherElement)
     element.classList.remove('hidden')
@@ -18,11 +19,26 @@ function getRandomAlphabet() {
 
     const Random = alphabet[index]
     return Random
+}
 
+// getTextElementValueById
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId)
+    const elementText = element.innerText
+    const value = parseInt(elementText)
+    return value
+}
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId)
+    element.innerText= value
 }
 
 // sets color of the alphabet
 function setColorOfTheAlphabet(elementId) {
-    const element= document.getElementById(elementId);
+    const element = document.getElementById(elementId);
     element.classList.add('bg-orange-300');
+}
+function removeColorOfTheAlphabet(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-300');
 }
